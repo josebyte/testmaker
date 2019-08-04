b6ecacec7125ce44e8fdee5f83ceaeb61e5bddff
Joseba <mjoseba@gmail.com>
2019-08-04 16:04:39 +0200

    Docker

---
+++ web/public/index2.php
@@ -0,0 +1,13 @@
+<!-- ./php/index.php -->
+
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello, World1!";
+        ?>
+    </body>
+</html>
